2104b12f3441e9641d3eede14bd741ccc7b498bd
Laura <laurajoyhutchins@gmail.com>
2026-08-24 09:53:29 -0600

    feat: wire om leaderboard adapter

--- src/opus_corpus/adapters/om_leaderboard.py
+++ src/opus_corpus/adapters/om_leaderboard.py
@@ -1,6 +1,7 @@
-from .base import SourceAdapter
+from .github import GitHubSourceAdapter
 
 
-class OmLeaderboardAdapter(SourceAdapter):
+class OmLeaderboardAdapter(GitHubSourceAdapter):
     source_id = "om-leaderboard"
     pinned_revision = "0cfd371ef66cf94eac3f7a7a06bc9ab959495576"
+    repository = "F43nd1r/om-leaderboard"
