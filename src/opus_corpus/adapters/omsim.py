e7b3c8540d790f2bcf928e12dcb266645080aeff
Laura <laurajoyhutchins@gmail.com>
2026-08-24 09:53:35 -0600

    feat: wire omsim adapter

--- src/opus_corpus/adapters/omsim.py
+++ src/opus_corpus/adapters/omsim.py
@@ -1,6 +1,7 @@
-from .base import SourceAdapter
+from .github import GitHubSourceAdapter
 
 
-class OmsimAdapter(SourceAdapter):
+class OmsimAdapter(GitHubSourceAdapter):
     source_id = "omsim"
     pinned_revision = "758f4a4b4c9e24f50294801da774a0960c922bab"
+    repository = "ianh/omsim"
