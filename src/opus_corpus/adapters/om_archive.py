d22e3de9bff52afc3c83793857df023229ca2333
Laura <laurajoyhutchins@gmail.com>
2026-08-24 09:53:22 -0600

    feat: wire om archive adapter

--- src/opus_corpus/adapters/om_archive.py
+++ src/opus_corpus/adapters/om_archive.py
@@ -1,6 +1,7 @@
-from .base import SourceAdapter
+from .github import GitHubSourceAdapter
 
 
-class OmArchiveAdapter(SourceAdapter):
+class OmArchiveAdapter(GitHubSourceAdapter):
     source_id = "om-archive"
     pinned_revision = "44006a0eeb0051337640443d1b0576ea24c983f6"
+    repository = "F43nd1r/om-archive"
