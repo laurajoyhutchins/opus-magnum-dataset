74948a1f07194fd50922c9a94804072162746274
Laura <laurajoyhutchins@gmail.com>
2026-08-24 09:53:17 -0600

    feat: wire molecule database adapter

--- src/opus_corpus/adapters/molecule_db.py
+++ src/opus_corpus/adapters/molecule_db.py
@@ -1,6 +1,7 @@
-from .base import SourceAdapter
+from .github import GitHubSourceAdapter
 
 
-class MoleculeDbAdapter(SourceAdapter):
+class MoleculeDbAdapter(GitHubSourceAdapter):
     source_id = "molecule-db"
     pinned_revision = "6f3cd8068428ef96ac6426d092c3523da359ec76"
+    repository = "fenhl/molecule-db"
