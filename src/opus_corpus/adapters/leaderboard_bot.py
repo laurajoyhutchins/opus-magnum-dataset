0ba04f90b4ede7e82b49d71cbce35c2e135b75b6
Laura <laurajoyhutchins@gmail.com>
2026-08-24 09:53:10 -0600

    feat: wire leaderboard bot adapter

--- src/opus_corpus/adapters/leaderboard_bot.py
+++ src/opus_corpus/adapters/leaderboard_bot.py
@@ -1,6 +1,7 @@
-from .base import SourceAdapter
+from .github import GitHubSourceAdapter
 
 
-class LeaderboardBotAdapter(SourceAdapter):
+class LeaderboardBotAdapter(GitHubSourceAdapter):
     source_id = "leaderboard-bot"
     pinned_revision = "ca40dee95da584270eb3be1c4b74e2be63afa7e6"
+    repository = "F43nd1r/zachtronics-leaderboard-bot"
